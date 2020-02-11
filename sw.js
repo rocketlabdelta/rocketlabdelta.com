6f96175cddb7b34ec9589af26440ac2e2ed73547
Dan Horst <danhorst@gmail.com>
2020-02-10 22:18:56 -0500

    Refining nav flow on small viewports

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ Thanks to [Jeremy][1], [Filament Group][2], and [Erik][3] for this, and everythi
 (function () {
   'use strict'
 
-  const version = 'v2020-02-07T22:35:00::'
+  const version = 'v2020-02-10T17:35:00::'
   const staticCacheName = version + 'static'
   const pagesCacheName = version + 'pages'
   const imagesCacheName = version + 'images'
