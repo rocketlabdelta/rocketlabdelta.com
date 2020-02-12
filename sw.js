9f71a535cd79886322dd6f690d2b38a3666a8b11
Dan Horst <danhorst@gmail.com>
2020-02-12 18:36:22 -0500

    One more round of header spacing

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ Thanks to [Jeremy][1], [Filament Group][2], and [Erik][3] for this, and everythi
 (function () {
   'use strict'
 
-  const version = 'v2020-02-12T16:00:00::'
+  const version = 'v2020-02-12T18:00:00::'
   const staticCacheName = version + 'static'
   const pagesCacheName = version + 'pages'
   const imagesCacheName = version + 'images'
