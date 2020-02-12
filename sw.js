d37020676f310674a84405ed85cc1a640160138e
Dan Horst <danhorst@gmail.com>
2020-02-12 12:08:16 -0500

    CSS classes can't start with a number

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ Thanks to [Jeremy][1], [Filament Group][2], and [Erik][3] for this, and everythi
 (function () {
   'use strict'
 
-  const version = 'v2020-02-12T11:57:00::'
+  const version = 'v2020-02-12T12:05:00::'
   const staticCacheName = version + 'static'
   const pagesCacheName = version + 'pages'
   const imagesCacheName = version + 'images'
