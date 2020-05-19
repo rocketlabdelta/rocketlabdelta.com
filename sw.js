e1ff3bd234295fae3dd1a24f4cd6af36d79b82a2
Dan Horst <danhorst@gmail.com>
2020-05-19 07:56:21 -0400

    Adding linter hint

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+/*  eslint-env serviceworker  */
+
 /*
 This is a modified version of the Service Worker from www.nd.edu created by Erik Runyon
 It, in turn, is based on Jeremy Keith’s service worker (https://adactio.com/serviceworker.js), with a few additional edits borrowed from Filament Group’s. (https://www.filamentgroup.com/sw.js)
